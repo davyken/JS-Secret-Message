0d68e26a7c940f4deab857d731539372e2af0fc8
davyken <davykennang552@gmail.com>
2024-02-20 15:43:45 +0100

    modified content

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,7 +20,7 @@ form.addEventListener("submit", (e) => {
     let arrOfNormalized = normalized.match(/.{1,8}/g);
     displayArr(arrOfNormalized, normText)
    
-   encodeButton.addEventListener("click", () => {
+   encodeButton.addEventListener("click", () => {//(https://github.com/davyken/JS-Secret-Handshake/pull/1)
     let encodedArr = [];
     for (let i = 0; i < arrOfNormalized.length; i++) {
       for (let j = 0; j < arrOfNormalized.length; j++) {
@@ -44,3 +44,42 @@ function displayArr (arr, mode) {
 }
 
 
+// let form = document.querySelector('form');
+// let normText = document.querySelector('#nt');
+// let enChunks = document.querySelector('#ec');
+// let enMessage = document.querySelector('#em');
+// let encodeButton = document.querySelector('#encode');
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   normText.textContent = '';
+//   normText.style.color = "white";
+//   let sentence = e.target.sentenceI.value;
+
+//   const normalized1 = sentence.replace(/[.,"'<>?"" """[ \/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, "");
+//   const normalized = normalized1.toLowerCase();
+//   let arrOfNormalized = normalized.match(/.{1,8}/g);
+//   displayArr(arrOfNormalized, normText);
+
+//   encodeButton.addEventListener("click", () => {
+//     let encodedArr = [];
+//     for (let i = 0; i < arrOfNormalized.length; i++) {
+//       for (let j = 0; j < arrOfNormalized.length; j++) {
+//         encodedArr.push(arrOfNormalized[j][i]);
+//       }
+//     }
+//     let encodedString1 = String(encodedArr);
+//     let encodedString = encodedString1.replaceAll(",", "");
+//     enChunks.textContent = encodedString;
+//     let arrOfEncoded = encodedString.match(/.{1,6}/g);
+//     displayArr(arrOfEncoded, enMessage);
+//   });
+// });
+
+
+// function displayArr(arr, mode) {
+//   mode.innerHTML = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     mode.innerHTML += '"' + arr[i] + '"' + "<br>";
+//   }
+// }
